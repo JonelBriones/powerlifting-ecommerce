f50da3f782ce298436d02e7f780037e42b1d3704
JonelBriones <jonel.c.briones@gmail.com>
2025-04-04 21:54:27 -0700

    added updated cart items and remove buttons

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import products from "@/data/products.json";
 import { Products, ProductT } from "@/_types/products";
 import Image from "next/image";
+import Link from "next/link";
 const page = () => {
   interface Cart {
     id: string;
@@ -39,10 +40,37 @@ const page = () => {
     setLoading(false);
   }, []);
 
+  const subtractQuantity = (id: number) => {
+    const removeItem = cart.find((product) => product.id === id);
+    if (removeItem.quantity > 1) {
+      setCart((prev) =>
+        prev.map((product) =>
+          product.id === id
+            ? { ...product, quantity: product.quantity - 1 }
+            : product
+        )
+      );
+    } else {
+      setCart((prev) => prev.filter((product) => product.id !== id));
+    }
+  };
+  const addQuantity = (id: number) => {
+    const productToUpdate = cart.map((product) =>
+      product.id === id
+        ? { ...product, quantity: product.quantity + 1 }
+        : product
+    );
+    setCart(productToUpdate);
+  };
+
   return (
-    <div>
-      <h1 className="text-2xl">CART</h1>
-      <span>Continue shopping</span>
+    <div className="flex flex-col gap-4 w-full  max-w-[1280px] m-auto p-4 flex-1">
+      <div className="text-center">
+        <h1 className="text-5xl">CART</h1>
+        <Link href={"/collections"} className="underline">
+          Continue shopping
+        </Link>
+      </div>
       {cart.length > 0 && (
         <div className="flex flex-col gap-2">
           {cart.map((item) => (
@@ -56,23 +84,46 @@ const page = () => {
                   sizes="100vw"
                 />
               </div>
-              <div className="flex flex-col">
-                <span>{item.name}</span>
-                <span>
-                  {item?.size && (
-                    <>
-                      <span className="font-medium">Size: </span>
-                      {item.size.toUpperCase()}
-                    </>
-                  )}
+              <div className="flex flex-col gap-2">
+                <span className="">{item.name}</span>
+                <span className=" text-neutral-400">
+                  ${(item.price * item.quantity).toFixed(2)}
                 </span>
-                <span>${item.price * item.quantity}</span>
-                <div className="flex justify-between border p-2 w-30">
-                  <button className="cursor-pointer">-</button>
-                  <span>{item.quantity}</span>
-                  <button className="cursor-pointer">+</button>
+                {item?.size && (
+                  <span className="font-medium text-neutral-400">
+                    Size: {item.size.toUpperCase()}
+                  </span>
+                )}
+                <div className="flex gap-4 place-items-center">
+                  <div className="block border border-neutral-300 p-2 gap-4">
+                    <button
+                      className="cursor-pointer w-5"
+                      onClick={() => subtractQuantity(item.id)}
+                    >
+                      -
+                    </button>
+                    <span className="w-10 text-center inline-block">
+                      {item.quantity}
+                    </span>
+                    <button
+                      className="cursor-pointer w-5"
+                      onClick={() => addQuantity(item.id)}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <button
+                    type="button"
+                    className="underline"
+                    onClick={() =>
+                      setCart((prev) =>
+                        prev.filter((product) => product.id !== item.id)
+                      )
+                    }
+                  >
+                    Remove
+                  </button>
                 </div>
-                <span>Remove</span>
               </div>
             </div>
           ))}
